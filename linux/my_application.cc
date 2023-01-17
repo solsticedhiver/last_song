5e43df3a2d76e6a9f5ba2c267b8608fc2532ac56
solsTiCe d'Hiver <solstice.dhiver@gmail.com>
2023-01-17 16:20:03 +0100

    Change title in title bar (linux gtk desktop)

--- linux/my_application.cc
+++ linux/my_application.cc
@@ -40,11 +40,11 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "last_song");
+    gtk_header_bar_set_title(header_bar, "Last Song");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    gtk_window_set_title(window, "last_song");
+    gtk_window_set_title(window, "Last Song");
   }
 
   gtk_window_set_default_size(window, 1280, 720);
