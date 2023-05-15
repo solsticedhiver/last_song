66000aab1a4a72953903dbdaa5a065285569f357
solsTiCe d'Hiver <solstice.dhiver@gmail.com>
2023-05-16 00:09:08 +0200

    Upgrade deprecated feature from web version

--- flutter-web.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# work-around to disable CORS causing error
-# TODO: hide the tab strip because there will be only one tab there
-flutter run -d chrome --web-browser-flag "--disable-web-security"
